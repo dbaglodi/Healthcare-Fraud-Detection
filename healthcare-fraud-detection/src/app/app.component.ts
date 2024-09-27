780487b6a1c583f622cd803f34ae6cb4c33194ab
saanvi.bhu <sbhumpalle@gmail.com>
2024-09-27 19:02:33 -0400

    attempt

--- healthcare-fraud-detection/src/app/app.component.ts
+++ healthcare-fraud-detection/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ModelInfoComponent } from './model-info/model-info.component';
+import { TeamComponent } from './team/team.component';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ModelInfoComponent,
+    TeamComponent,
+    LoginComponent,
+    CreateAccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  title = 'healthcare-fraud-detection';
-}
+export class AppModule { }
