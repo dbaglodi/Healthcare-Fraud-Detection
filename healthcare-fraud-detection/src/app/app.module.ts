fa03e90dc38312f55607e5999e57e49e7c6960fa
dbaglodizavvie <disha.baglodi@zavvie.com>
2024-09-28 10:10:09 -0400

    i think i fixed something

--- healthcare-fraud-detection/src/app/app.module.ts
+++ healthcare-fraud-detection/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 //import { AppComponent } from './app.component';
-import { AppComponent } from './components/app.component';
+import { AppComponent } from './app.component';
 
 import { HomeComponent } from './home/home.component';
 import { ModelInfoComponent } from './model-info/model-info.component';
